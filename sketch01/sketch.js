c6818b38c83871d1b0bb53b94563e865224b7fcc
hatanotomoki <tomoki2021tt@nzu.jp>
2022-04-19 16:13:06 +0900

    模様　作り始める

--- sketch01/sketch.js
+++ sketch01/sketch.js
@@ -7,14 +7,18 @@ window.addEventListener('load', function() {
     // キャンバスの大きさを指定する。
     view.viewSize = new Size(500, 500);
 
-    let x = view.viewSize.width * 0.25;
-    let y = view.viewSize.height * 0.3;
-    let rectSize = [200, 300];
+    project.currentStyle = {
+        strokeColor: '#000000', // 線の色
+        fillColor: '#fbff00', // 塗りの色
+        strokeWidth: 1 // 線の幅
+    };
+   Path.Rectangle([0,0],[500,500])
 
-    // 長方形を追加する。
-    let rect = new Path.Rectangle([x, y], rectSize);
-    rect.strokeColor = 'black';
-
+   for (let a = 10; a<500; a += 20)[Path.Line([a,0],[a,500])]
+   
+   Path.Line([a,0],[a,500])
+   let line = Path.Line([a,0],[a,500])
+   line.strokeColor= '#ff0000' 
     // 画面を描く。
     view.draw();
 });
